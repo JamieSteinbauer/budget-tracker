151492f6b50696f0f304c0ef062c872b912f31e8
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-07-14 17:59:31 -0400

    created manifest.json

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
-const e = require("express");
+
 
 const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_01-';
+const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
@@ -20,8 +20,8 @@ const FILES_TO_CACHE = [
     './icons/icon-72x72.png',
 ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
+self.addEventListener('install', function(e) {
+    e.waitUntil(
         caches.open(CACHE_NAME).then(function(cache) {
             console.log('installing cache: ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
@@ -29,8 +29,8 @@ self.addEventListener('install', function(event) {
     )
 })
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
         caches.keys().then(function(keyList) {
             let cacheKeepList = keyList.filter(function(key) {
                 return key.indexOf(APP_PREFIX);
@@ -46,16 +46,16 @@ self.addEventListener('activate', function(event) {
     )
 })
 
-self.addEventListener('fetch', function(event) {
-    console.log('fetch request: ' + event.request.url)
-    event.respondWith(
-        caches.match(event.request).then(function(request) {
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request: ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function(request) {
             if(request) {
-                console.log('responding with cache: ' + event.request.url)
+                console.log('responding with cache: ' + e.request.url)
                 return request
             } else {
-                console.log('file is not cached... fetching: ' + event.request.url)
-                return fetch(event.request)
+                console.log('file is not cached... fetching: ' + e.request.url)
+                return fetch(e.request)
             }
         })
     )
